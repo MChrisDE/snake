8edbc6551b6ad67b6f721be4fa35126aa66e1666
Stefan Lukas <afc11hn@hotmail.de>
2017-07-23 23:03:28 +0200

    Reorder constants

--- constants.py
+++ constants.py
@@ -1,7 +1,23 @@
 import pygame
 
-WHITE = 255, 255, 255
-BLACK = 0, 0, 0
 
-pygame.font.init()
-DEFAULT_FONT40 = pygame.font.SysFont("monospace", 40)  # set after pygame.init()
+class Colors:
+	GREY = 50, 50, 50
+	WHITE = 255, 255, 255
+	BLACK = 0, 0, 0
+
+
+class Fonts:
+	pygame.font.init()
+	DEFAULT_FONT40 = pygame.font.SysFont("monospace", 40)  # set after pygame.init()
+
+	@staticmethod
+	def default(size):
+		return pygame.font.SysFont("monospace", size)
+
+
+class MoveDirections:
+	UP = {pygame.K_UP, pygame.K_w}
+	LEFT = {pygame.K_LEFT, pygame.K_a}
+	DOWN = {pygame.K_DOWN, pygame.K_s}
+	RIGHT = {pygame.K_RIGHT, pygame.K_d}
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from snake import start_game
+from game import start_game
 from constants import *
 
 buttons = []
 
 
 class Button:
-	def __init__(self, text, x, y, font=DEFAULT_FONT40, fn=lambda: None):
-		self.text = font.render(text, True, BLACK)
+	def __init__(self, text, x, y, font=Fonts.DEFAULT_FONT40, fn=lambda: None):
+		self.text = font.render(text, True, Colors.BLACK)
 		self.rect = self.text.get_rect()
 		self.rect.center = (x, y)
 		self.run = fn
@@ -18,7 +18,7 @@ class Button:
 
 
 def create_buttons():
-	Button("Snake", 250, 100)
+	Button("Snake", 250, 100, font=Fonts.default(size=80))
 	Button("Start game", 250, 250, fn=start)
 	Button("Exit Game", 250, 400, fn=exit_game)
 
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
 	root = pygame.display.set_mode((500, 500))
 	pygame.display.set_caption("Snake Menu")
-	root.fill(WHITE)
+	root.fill(Colors.WHITE)
 
 	create_buttons()
 
