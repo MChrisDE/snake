eefa8eb836b0363262f3d0eef02f5212a5eac5db
Stefan Lukas <afc11hn@hotmail.de>
2017-07-25 15:25:39 +0200

    Update str

--- snake.py
+++ snake.py
@@ -15,6 +15,9 @@ class Snake(list):
 				self.is_head = pos['head']
 				del pos['head']
 
+		def __str__(self):
+			return str(self.get_x()) + ',' + str(self.get_y())
+
 	def __init__(self, root, **initial):
 		super().__init__()
 		self.length = initial.get('length', 3)
@@ -27,4 +30,4 @@ class Snake(list):
 			self.append(Snake.Part(self.head_position.get_y() - i, self.head_position.get_y()))
 
 	def __str__(self):
-		return f'Snake: {";".join([str(part) for part in self])}'
+		return f'Snake: {";".join(map(str, self))}'
